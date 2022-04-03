7405c906b84ac5d0bcae0cd833e9f94fb3d17ae5
conceptblend <the.concept.blend@gmail.com>
2022-04-03 17:56:57 -0600

    refactor: clean up stale comments in presets

--- src/presets.js
+++ src/presets.js
@@ -1,13 +1,3 @@
-// const LIFESPAN = CONFIG.lifespan; // 48, 96, 200
-// const /*tree.*/MAXDIST = CONFIG.maxDist; // 48, 24
-// Open when min is greater than BRANCH_LENGTH, closed when smaller
-// const /*tree.*/MINDIST = CONFIG.minDist; // 8 
-// const BRANCH_LENGTH = CONFIG.branchLength;
-// const /*tree.*/N = CONFIG.angle; // angle in DEGREES: 90 and 120 make good geometry
-  // ^^^ Also tried weird values like 75 deg and 15 deg
-// const CANVASSIZE = CONFIG.canvasSize;
-// const ATTRACTORCOUNT = CONFIG.attractors; // 25000
-
 const presets = {
   zero: {
     lifespan: 48,
