0e9415cf221d1b6ef922ba4ee66fb2bd9141966c
conceptblend <the.concept.blend@gmail.com>
2022-05-17 08:48:26 -0600

    refactor(util): added more info to preplot script

--- preplot.sh
+++ preplot.sh
@@ -14,13 +14,18 @@ if [[ $1 ]] then
     FILE="occ.svg"
   fi
 
+  echo "Processing..."
+
   # Note: I removed `splitall` from the pipeline because the blobs are better maintained and occluded without it
   vpype -v read --attr fill $1 reloop linemerge linesimplify occult -i -r linesort layout --align center --valign center --fit-to-margins 0.05in 5.5inx8.5in filter --min-length 1mm write -f svg $FILE
+  # vpype -v read --attr fill $1 reloop linemerge linesimplify occult -r linesort layout --align center --valign center --fit-to-margins 0.05in 5.5inx8.5in filter --min-length 1mm write -f svg $FILE
   
   echo "Done."
   echo "Opening $FILE..."
 
   open $FILE
 else
-    echo "Missing expected parameter."
+  echo "Missing expected parameter.\n"
+  echo "Usage:"
+  echo "$> ./preplot.sh input.svg output.svg"
 fi
