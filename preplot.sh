b26b67c442243a5ce74e06759dfd14895ea0d157
conceptblend <the.concept.blend@gmail.com>
2023-03-04 19:25:32 -0700

    build: fix saved commands

--- preplot.sh
+++ preplot.sh
@@ -16,16 +16,16 @@ if [[ $1 ]] then
 
   echo "Processing..."
 
-  # Note: I removed `splitall` from the pipeline because the blobs are better maintained and occluded without it
+  # Note: I can't decide if `splitall` creates better blobs or not
 
   # IGNORE LAYERS
-  vpype -v read --attr fill $1 reloop linemerge linesimplify occult -i -r linesort layout --align center --valign center --fit-to-margins 0.05in 5.5inx8.5in filter --min-length 1mm write -f svg $FILE
+  # vpype -v read $1 reloop linemerge linesimplify occult -i -r linesort layout --align center --valign center --fit-to-margins 0.05in 5.5inx8.5in filter --min-length 1mm write -f svg $FILE
 
   # IGNORE LAYERS AND DON'T LAYER SORT
   # vpype -v read $1 reloop linemerge linesimplify occult -i linesort layout --align center --valign center --fit-to-margins 0.05in 5.5inx8.5in filter --min-length 1mm write -f svg $FILE
   
   # RESPECT LAYERS
-  # vpype -v read --attr fill $1 reloop linemerge linesimplify occult -r linesort layout --align center --valign center --fit-to-margins 0.05in 5.5inx8.5in filter --min-length 1mm write -f svg $FILE
+  vpype -v read --attr fill $1 splitall reloop linemerge linesimplify occult -r linesort layout --align center --valign center --fit-to-margins 0.05in 5.5inx8.5in filter --min-length 1mm write -f svg $FILE
   
   echo "Done."
   echo "Opening $FILE..."
